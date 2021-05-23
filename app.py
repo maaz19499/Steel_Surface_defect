c5ac4129c8f73d8c65f3451bf6d55a6781d3d413
MOHAMMED MAAZ <63781208+maaz19499@users.noreply.github.com>
2021-05-23 10:57:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ import tensorflow
 from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
+import PIL
 
 # Flask utils
 from flask import Flask, redirect, url_for, request, render_template
